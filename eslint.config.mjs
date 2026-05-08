3e417a1becb2ffb9c6613df73e4325c6dbc5be03
AluronAluron <alurondev@gmail.com>
2026-05-08 15:15:17 -0600

    feat: css, eslint and prettier configs

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+export default [
+  ...eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: { prettier: eslintPluginPrettier },
+    rules: {
+      'prettier/prettier': 'warn'
+    }
+  }
+];
